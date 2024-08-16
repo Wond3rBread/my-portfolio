5ef83731c1ce89f3dc6f1b7f56848d55ceb8eaa9
Bengt <barothei@ntnu.no>
2024-08-17 00:31:53 +0200

    Potenital landing svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import { demos } from '#/lib/demos';
 import Link from 'next/link';
+import bg from 'assets/mountainrangebg.svg';
 
 export default function Page() {
   return (
-    <div>
-      <h1 className=" font-playfair text-4xl font-bold">Hei</h1>
+    <div
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh', // Make sure the div takes up the full height of the viewport
+        width: '100%',
+      }}
+    >
+      <h1 className="font-playfair text-4xl font-bold text-white">Hei</h1>
     </div>
   );
 }
